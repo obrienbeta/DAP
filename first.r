a90883a96911c2a5629f4d4ca912528d1e762e88
obrienbeta <obrienbeta@gmail.com>
2017-10-04 20:39:41 +0100

    First R script

---
+++ first.r
@@ -0,0 +1,17 @@
+#Correlation coefficient analysis
+
+x <- c(2,3,5,7,9,7,6,7,5,8,4)
+
+# y is the dependant variable
+
+y <- c(6,3,4,6,7,6,2,2,7,8,5)
+
+res <- cor(x,y)
+
+
+print(res)
+
+#Correlation coefficient
+#is order dependent and movinf the order
+# around affects the coefficient calculation
+
